8823f369fd3f48d957f2a23e4fbcd515a4481df4
Sallykamel <106214987+Sallykamel@users.noreply.github.com>
2024-05-13 22:42:47 +0300

    Update Main.java

--- RMI/src/main/java/Main.java
+++ RMI/src/main/java/Main.java
@@ -4,9 +4,11 @@ import Client.Threads;
 
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
+        deleteLogFiles();
         System.out.println("starting server firstly");
         Server.startServer();
         System.out.println("Then Starting client");
@@ -16,5 +18,18 @@ public class Main {
         }
 
 
+    }
+    private static void deleteLogFiles() {
+    // Delete log files
+    for (int i = 0; i < Threads.noOfClients; i++) {
+        String logFileName = "log" + i + ".txt";
+        File logFile = new File(logFileName);
+        if (logFile.exists()) {
+            logFile.delete();
+            System.out.println("Deleted log file: " + logFileName);
+        } else {
+            System.out.println("Log file does not exist: " + logFileName);
+        }
+    }
     }
 }
