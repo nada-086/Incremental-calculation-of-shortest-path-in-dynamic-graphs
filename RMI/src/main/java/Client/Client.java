7b270b9bdc4953ed60b897f69906d9f1b962e11b
Toka Ashraf <101206117+TokaAshraf12@users.noreply.github.com>
2024-05-17 15:33:39 +0300

    add performance of write percentage

--- RMI/src/main/java/Client/Client.java
+++ RMI/src/main/java/Client/Client.java
@@ -38,6 +38,37 @@ public class Client extends Thread implements Runnable {
         }
 
     }
+     public static void performanceWritePercentage() throws IOException, NotBoundException {
+        int graphSize = 6;
+        int[] writePercentage =  {30, 50, 70, 90};
+        int batchSize = 4;
+        Registry registry = LocateRegistry.getRegistry("127.0.1.1",1099);
+        IRemoteMethod reg = (IRemoteMethod) registry.lookup("RemoteMethod");
+        long[] median = new long[5];
+        int noOfReadings = 5;
+        for (int k : writePercentage) {
+           // long sumTime = 0;
+            for (int j = 0; j < noOfReadings; j++) {
+                ArrayList<String> batch = getBatch(k, batchSize, graphSize);
+                System.out.println("batch generated is:  \n" + batch);
+                if (reg.serverReady()) {
+                    long startTime = System.currentTimeMillis();
+                    ArrayList<String> response = reg.processBatch(batch, "BFS");
+                    long endTime = System.currentTimeMillis();
+
+                    long responseTime = endTime - startTime;
+                    System.out.println("response time : "+ responseTime);
+                   // sumTime += responseTime;
+                    median[j] = responseTime;
+                }
+
+            }
+            Arrays.sort(median);
+            System.out.println("average response time for write percentage :" + k
+                    + " and 10 readings equals: " + (median[noOfReadings/2]) + "ms");
+
+        }
+    }
     
 private static void logToFile(String fileName, ArrayList<String> batch, ArrayList<String> responses, long responseTime) {
     try {
@@ -70,7 +101,8 @@ private static void logToFile(String fileName, ArrayList<String> batch, ArrayLis
     @Override
     public void run() {
         try {
-            startClientProcess();
+           //            startClientProcess();
+            performanceWritePercentage();
         } catch (RemoteException | NotBoundException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
